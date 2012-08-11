f17eaa2f6cc3ef1a5cbd8575440b1e13fb99c3f3
secret <siddhartha047@gmail.com>
2012-08-11 19:51:11 +0600

    i created a static folder and two header and footer

---
+++ application/views/footer/sid_footer.php
@@ -0,0 +1,28 @@
+
+			<div id="halogycms_browser" class="loading"></div>
+
+		</div>
+
+	</div>
+
+
+	<div id="footer" class="content">
+
+		<div class="container">
+
+		
+			<p class="copyright">Powered by <a href="#" title="Halogy">CMS buet</a></p>
+			
+			<br />
+			
+                        <a href="#"><img src="<?php echo base_url();?>images/cms_logo.png" alt="have to give a logo" width="50px" height="50px"/></a>
+
+
+		</div>
+
+	</div>
+
+</div>
+	
+</body>
+</html>
