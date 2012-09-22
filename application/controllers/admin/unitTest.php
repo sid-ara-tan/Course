dea77689a0be738835327783e679e9c0d6db5ec3
secret <siddhartha047@gmail.com>
2012-09-23 00:33:53 +0600

    no

---
+++ application/controllers/admin/unitTest.php
@@ -0,0 +1,36 @@
+<?php class UnitTest extends CI_Controller{
+
+    function  __construct() {
+        parent::__construct();
+        $this->my_library->check_logged_in();
+    }
+
+    function sample(){
+        echo 'This is sample test';
+        $test = $this->getI(4);
+
+        $expected_result = 16;
+
+        $test_name = 'Adds one plus one';
+
+        echo $this->unit->run($test, $expected_result, $test_name);
+       
+        /*echo br();
+        echo $this->unit->result();
+        echo br();
+        echo $this->unit->report();
+
+        echo br();
+        echo $this->unit->run('Foo', 'Foo');
+        echo br();
+        echo $this->unit->run('Foo', 'is_string');*/
+    }
+    
+    
+    function getI($i=5){
+        return $i*$i;
+    }
+
+
+
+}
